09dda8eb308119d459bee5892ecae235a7c5570d
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-02-15 17:47:05 +0530

    Format

--- script.js
+++ script.js
@@ -9,222 +9,226 @@ var filter = "all";
 status.innerHTML = "Fetching...";
 
 function filterProviders() {
-  $("li").each(function () {
-    if (filter === "all") {
-      $(this).show();
-    } else if (
-      $(this)
-        .find(".siteValues")
+ $("li").each(function () {
+  if (filter === "all") {
+   $(this).show();
+  } else if (
+   $(this)
+    .find(".siteValues")
 
-        .text()
+    .text()
 
-        .toLowerCase()
+    .toLowerCase()
 
-        .indexOf(filter.toLowerCase()) === -1
-    ) {
-      $(this).hide();
-    } else {
-      $(this).show();
-    }
-  });
+    .indexOf(filter.toLowerCase()) === -1
+  ) {
+   $(this).hide();
+  } else {
+   $(this).show();
+  }
+ });
 }
 
 $("input[name='filter']").on("change", function () {
-  filter = this.value;
+ filter = this.value;
 
-  filterProviders();
+ filterProviders();
 });
 
 $(document).ready(function () {
-  $.getJSON("https://raw.githubusercontent.com/OshekharO/Web-Indexer/main/providers.json", function (data) {
-    status.innerHTML = "Parsing...";
+ $.getJSON("https://raw.githubusercontent.com/OshekharO/Web-Indexer/main/providers.json", function (data) {
+  status.innerHTML = "Parsing...";
 
-    for (var key in data) {
-      status.innerHTML = "Reading..." + key;
+  for (var key in data) {
+   status.innerHTML = "Reading..." + key;
 
-      if (data.hasOwnProperty(key)) {
-        var value = data[key];
+   if (data.hasOwnProperty(key)) {
+    var value = data[key];
 
-        if (value.url == "NONE") {
-          continue;
-        }
-
-        var _status = value.status;
-
-        var _thumbNail = value.url.trimRight("/") + "/favicon.ico";
-
-        //Use defaut image if missing
-
-        if (value.hasOwnProperty("icon")) {
-          _thumbNail = value.icon;
-        }
-
-        //Create <li> node
-
-        var node = document.createElement("li");
-
-        //Add <div> to <li> node for items
-
-        var _divMain = document.createElement("div"); //group below divs
-
-        var _divImg = document.createElement("div"); //for image Thumbnail
-
-        var _divText = document.createElement("div"); //for sitename + status
-
-        var _img = document.createElement("img"); //Image thumbnail
-
-        //Add image
-
-        _img.className = "thumbnail";
-
-        _img.style.content = "";
-
-        _img.src = _thumbNail;
-
-        _img.loading = "lazy"; // Add this line to enable lazy loading
-
-         //_img.src = "https://via.placeholder.com/150";
-
-        //_img.src = value.url.trimRight("/") + "/favicon.ico";
-
-        //Modify _divImg
-
-        _divImg.className = "divthumb";
-
-        _divImg.appendChild(_img);
-
-        _divMain.appendChild(_divImg);
-
-        //Add siteName hyperlink text
-
-        var _a = document.createElement("a");
-
-        _a.setAttribute("href", value.url);
-
-        _a.innerHTML = value.name;
-
-        _a.title = key;
-
-        _a.style.color = "#fff";
-
-        _a.className = "siteName";
-
-        _divText.appendChild(_a);
-
-        //Add status label
-
-        var _p = document.createElement("p");
-
-        _p.innerHTML = "Type: ";
-
-        _p.className = "siteLabels";
-
-        //Add status from json
-
-        var _span = document.createElement("span");
-
-        _span.className = "siteValues";
-
-        var _statusText = "Unknown";
-
-        var _statusColor = "#eee";
-
-        switch (_status) {
-          case 0:
-            _statusText = "NSFW";
-
-            _statusColor = "#e57373";
-
-            break;
-
-          case 1:
-            _statusText = "MANGA";
-
-            _statusColor = "#8bc34a";
-
-            break;
-
-          case 2:
-            _statusText = "LN";
-
-            _statusColor = "#ffc107";
-
-            break;
-
-          case 3:
-            _statusText = "MOVIE";
-
-            _statusColor = "#64b5f6";
-
-            break;
-
-          case 4:
-            _statusText = "APP";
-
-            _statusColor = "#64b5f6";
-
-            break;
-
-          case 5:
-            _statusText = "ANIME";
-
-            _statusColor = "#64b5f6";
-
-            break;
-        }
-
-        _span.style.color = _statusColor;
-
-        _span.textContent = _statusText;
-
-        _p.appendChild(_span);
-
-        //Add (status + status label) to _divText
-
-        _divText.appendChild(_p);
-
-        //Add type, if it exists
-
-        if (value.hasOwnProperty("type")) {
-          var _pType = document.createElement("p");
-
-          _pType.innerHTML = "Type: ";
-
-          _pType.className = "siteLabels";
-
-          var _spanType = document.createElement("span");
-
-          _spanType.className = "siteValues";
-
-          _spanType.textContent = value.type;
-
-          _pType.appendChild(_spanType);
-
-          _divText.appendChild(_pType);
-        }
-
-        //Add all texts for entry
-
-        _divText.className = "divcaption";
-
-        _divMain.appendChild(_divText);
-
-        //Add main container to node
-
-        _divMain.className = "divMainContainer";
-
-        node.appendChild(_divMain);
-
-        //Add <li> to <ul> siteList
-
-        mainContainer.appendChild(node);
-      }
+    if (value.url == "NONE") {
+     continue;
     }
-  }).fail(function () {
-    console.log("An error has occurred.");
 
-    status.innerHTML = "Error occured!";
-  });
+    var _status = value.status;
+
+    var _thumbNail = value.url.trimRight("/") + "/favicon.ico";
+
+    //Use defaut image if missing
+
+    if (value.hasOwnProperty("icon")) {
+     _thumbNail = value.icon;
+    }
+
+    //Create <li> node
+
+    var node = document.createElement("li");
+
+    //Add <div> to <li> node for items
+
+    var _divMain = document.createElement("div"); //group below divs
+
+    var _divImg = document.createElement("div"); //for image Thumbnail
+
+    var _divText = document.createElement("div"); //for sitename + status
+
+    var _img = document.createElement("img"); //Image thumbnail
+
+    //Add image
+
+    _img.className = "thumbnail";
+
+    _img.style.content = "";
+
+    _img.src = _thumbNail;
+
+    _img.loading = "lazy"; // Add this line to enable lazy loading
+
+    _img.onerror = function () {
+     _img.src = "https://via.placeholder.com/150"; // Use a default image if loading fails
+    };
+
+    //_img.src = "https://via.placeholder.com/150";
+
+    //_img.src = value.url.trimRight("/") + "/favicon.ico";
+
+    //Modify _divImg
+
+    _divImg.className = "divthumb";
+
+    _divImg.appendChild(_img);
+
+    _divMain.appendChild(_divImg);
+
+    //Add siteName hyperlink text
+
+    var _a = document.createElement("a");
+
+    _a.setAttribute("href", value.url);
+
+    _a.innerHTML = value.name;
+
+    _a.title = key;
+
+    _a.style.color = "#fff";
+
+    _a.className = "siteName";
+
+    _divText.appendChild(_a);
+
+    //Add status label
+
+    var _p = document.createElement("p");
+
+    _p.innerHTML = "Type: ";
+
+    _p.className = "siteLabels";
+
+    //Add status from json
+
+    var _span = document.createElement("span");
+
+    _span.className = "siteValues";
+
+    var _statusText = "Unknown";
+
+    var _statusColor = "#eee";
+
+    switch (_status) {
+     case 0:
+      _statusText = "NSFW";
+
+      _statusColor = "#e57373";
+
+      break;
+
+     case 1:
+      _statusText = "MANGA";
+
+      _statusColor = "#8bc34a";
+
+      break;
+
+     case 2:
+      _statusText = "LN";
+
+      _statusColor = "#ffc107";
+
+      break;
+
+     case 3:
+      _statusText = "MOVIE";
+
+      _statusColor = "#64b5f6";
+
+      break;
+
+     case 4:
+      _statusText = "APP";
+
+      _statusColor = "#64b5f6";
+
+      break;
+
+     case 5:
+      _statusText = "ANIME";
+
+      _statusColor = "#64b5f6";
+
+      break;
+    }
+
+    _span.style.color = _statusColor;
+
+    _span.textContent = _statusText;
+
+    _p.appendChild(_span);
+
+    //Add (status + status label) to _divText
+
+    _divText.appendChild(_p);
+
+    //Add type, if it exists
+
+    if (value.hasOwnProperty("type")) {
+     var _pType = document.createElement("p");
+
+     _pType.innerHTML = "Type: ";
+
+     _pType.className = "siteLabels";
+
+     var _spanType = document.createElement("span");
+
+     _spanType.className = "siteValues";
+
+     _spanType.textContent = value.type;
+
+     _pType.appendChild(_spanType);
+
+     _divText.appendChild(_pType);
+    }
+
+    //Add all texts for entry
+
+    _divText.className = "divcaption";
+
+    _divMain.appendChild(_divText);
+
+    //Add main container to node
+
+    _divMain.className = "divMainContainer";
+
+    node.appendChild(_divMain);
+
+    //Add <li> to <ul> siteList
+
+    mainContainer.appendChild(node);
+   }
+  }
+ }).fail(function () {
+  console.log("An error has occurred.");
+
+  status.innerHTML = "Error occured!";
+ });
 });
 
 status.innerHTML = "Done loading!";
